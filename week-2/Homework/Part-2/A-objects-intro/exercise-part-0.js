b5b0aca682a46e30ce606ab41389642456d9a207
Rachid Essantouh <rachid.essantouh@gmail.com>
2022-05-12 12:21:42 +0200

    Exercise A done

--- week-2/Homework/Part-2/A-objects-intro/exercise-part-0.js
+++ week-2/Homework/Part-2/A-objects-intro/exercise-part-0.js
@@ -8,6 +8,13 @@ Try to think of as many properties as you can!
 
 let laptop = {
     brand: "Lenovo",
-    screenSize: 13,
+    model: "Pavilion dv6",
+    screenSize: 15,
+    RAM: "8gb",
+    CPU: "Intel Core2 Duo T9900 3.06 GHz",
+    GPU: "nVIDIA GeForce 9200M",
+    screenResolution: "1920 x 1080",
+    storageType: "SSD",
+    storage: "1To",
     isTouchscreen: true,
   };
--- week-2/Homework/Part-2/A-objects-intro/exercise-part-1.js
+++ week-2/Homework/Part-2/A-objects-intro/exercise-part-1.js
@@ -17,3 +17,42 @@ var car = {
 
 */
 
+var cat = {
+    name: "Shiba",
+    furColor: "black",
+    breed: "exotic shorthair",
+    specialty: ["sleeping", "meowing"],
+    favoriteFood: "tuna",
+}
+
+var france = {
+    continent: "Europe",
+    population: "67mil",
+    president: "Emmanuel Macron",
+    language: "French",
+    typicalFood: "boeuf bourgignon"
+}
+
+var Shrek = {
+    species: "ogre",
+    favoriteFood: "onions",
+    bestFriend: "Donkey",
+    residence: "swamp",
+    spouse: "Fiona"
+}
+
+var CrashTeamRacing = {
+    releaseDate: "2000",
+    console: "Sony Playstation",
+    numberPlayers: "up to 4 players",
+    typeOfGame: "car race",
+    recommendedAge: "12",
+}
+
+var celebrity = {
+    fullName : "Brad Pitt",
+    age: 58,
+    countryOfBirth: "USA",
+    profession : "actor",
+    netWorth : "300mil $"
+}
