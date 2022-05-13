cd91ece769b520467ce646f9013a5724d4db54c8
Rachid Essantouh <rachid.essantouh@gmail.com>
2022-05-13 23:30:29 +0200

    Exercise A done

--- week-3/Homework/A-timeout/exercise.js
+++ week-3/Homework/A-timeout/exercise.js
@@ -9,3 +9,17 @@ Task 2
 Update your code to make the colour change every 5 seconds to something different. Hint: try searching for setInterval. (https://www.w3schools.com/jsref/met_win_setinterval.asp)
 ================
 */
+
+const background = document.querySelector("body")
+
+setTimeout(() => {
+    background.style.backgroundColor = "purple"
+}, 5000)
+
+setInterval(() => {
+    if (background.style.backgroundColor === "pink") {
+      background.style.backgroundColor = "purple";
+    } else {
+      background.style.backgroundColor = "pink";
+    }
+}, 5000);
